d5c3b48fcfff5f72ddce2f4db7707fdccb2421e1
YankovichKirill <kirill.best97@gmail.com>
2022-09-11 19:29:32 +0300

    Save1

---
+++ Example1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write($"Enter the number M(number of numbers): ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] massiveNumbers = new int[m];
+
+void InputNumbers(int m){
+    for (int i = 0; i < m; i++){
+        Console.Write($"Enter {i + 1} number: ");
+        massiveNumbers[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+int Comparison(int[] massiveNumbers){
+    int count = 0;
+    for (int i = 0; i < massiveNumbers.Length; i++){
+        if (massiveNumbers[i] > 0 ) count += 1;
+    }
+    return count;
+}
+
+InputNumbers(m);
+
+Console.WriteLine($"Entered numbers greater than 0: {Comparison(massiveNumbers)} ");
